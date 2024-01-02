f7f7fc3770fdd5e05068923f712518db3076303f
Dario <dario.serrano@bghtechpartner.com>
2024-01-02 17:33:34 -0300

    test

--- alprestamo/infra/modules/ec2-alb/outputs.tf
+++ alprestamo/infra/modules/ec2-alb/outputs.tf
@@ -5,8 +5,3 @@ output "alb_dns_name" {
 output "alb_hosted_zone_id" {
     value =  aws_lb.application_load_balancer.zone_id
 }
-
-#output "webserver_public_ip" {
-#  value = aws_instance.webserver.public_ip
-#}
-
